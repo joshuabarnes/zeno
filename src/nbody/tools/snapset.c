358ddf05170737112f9f1a5e0ce5c81561eda88c
Joshua Barnes <barnes@ifa.hawaii.edu>
2015-02-02 15:04:43 +0900

    Added support for keyarr and auxarr variables.  Replaced tempnam() with mktemp().

--- src/nbody/tools/snapset.c
+++ src/nbody/tools/snapset.c
@@ -12,23 +12,22 @@
 #include <assert.h>
 
 string defv[] = {               ";Set values for each body in snapshot.",
-				";Expressions are written in C language.",
-				";Bound variables, depending on input, are:",
+				";Expressions (C code) may use these values:",
 				  SNAPMAP_BODY_VARS ".",
   "in=???",                     ";Input snapshot file name",
   "out=???",                    ";Output snapshot file name",
   "times=all",                  ";Range of times to process",
-  "x=",				";Expression for X position",
-  "y=",				";Expression for Y position",
-  "z=",				";Expression for Z position",
-  "vx=",			";Expression for X velocity",
-  "vy=",			";Expression for Y velocity",
-  "vz=",			";Expression for Z velocity",
+  "x=",				";Expression for x position",
+  "y=",				";Expression for y position",
+  "z=",				";Expression for z position",
+  "vx=",			";Expression for x velocity",
+  "vy=",			";Expression for y velocity",
+  "vz=",			";Expression for z velocity",
+  "ax=",			";Expression for x acceleration",
+  "ay=",			";Expression for y acceleration",
+  "az=",			";Expression for z acceleration",
   "m=",				";Expression for mass",
   "phi=",                       ";Expression for potential",
-  "ax=",			";Expression for X acceleration",
-  "ay=",			";Expression for Y acceleration",
-  "az=",			";Expression for Z acceleration",
   "smooth=",			";Expression for smoothing length",
   "rho=",			";Expression for density",
   "entf=",			";Expression for entropy function",
@@ -41,18 +40,20 @@ string defv[] = {               ";Set values for each body in snapshot.",
   "birth=",			";Expression for birth date",
   "death=",			";Expression for death date",
   "key=",			";Expression for key value",
+  "keyarr=",			";Expressions for key array",
   "aux=",                       ";Expression for aux value",
   "auxvx=",                     ";Expression for X aux vector",
   "auxvy=",                     ";Expression for Y aux vector",
   "auxvz=",                     ";Expression for Z aux vector",
-  "tmap=",			";Expression used to map time.",
-				";Bound variables are: "
+  "auxarr=",			";Expressions for aux array",
+  "t=",				";Expression used to map time.",
+				";May use these values: "
 				  SNAPMAP_TIME_VARS ".",
   "require=",                   ";List of input items necessary",
   "produce=",                   ";List of output items produced",
   "passall=true",		";If true, pass on all input data",
-  "seed=",			";Seed for random number generator",
-  "VERSION=2.8",                ";Josh Barnes  9 Sep 2014",
+  "seed=",			";Generator seed for random values",
+  "VERSION=2.9",                ";Josh Barnes  2 February 2015",
   NULL,
 };
 
@@ -61,32 +62,34 @@ void execmap(string prog);
 int main(int argc, string argv[])
 {
   string *mdtab, *names, *exprs, prog;
-  int i, j;
+  int nexp = 0, i, j;
 
   initparam(argv, defv);
-  mdtab = getmapdefs();
-  for (i = 0; mdtab[i] != NULL; i++)
-    continue;
-  assert((i & 1) == 0);
-  names = (string *) allocate(sizeof(string *) * i / 2);
-  exprs = (string *) allocate(sizeof(string *) * i / 2);
+  mdtab = getmapdefs();				// get list of mapping vars
+  for (i = 0; mdtab[i] != NULL; i += 2)
+    if (getparamstat(mdtab[i]) & ARGPARAM)	// if var has assigned value
+      nexp++;
+  eprintf("[%s: %scounted %d variable assignments]\n", getprog(),
+	  nexp > 0 ? "" : "warning: ", nexp);
+  names = (string *) allocate(sizeof(string *) * (nexp + 1));
+  exprs = (string *) allocate(sizeof(string *) * (nexp + 1));
   for (i = j = 0; mdtab[i] != NULL; i += 2)
-    if (getparamstat(mdtab[i]) & ARGPARAM) {	// if ident has been assigned
-      exprs[j] = getparam(mdtab[i]);		// list given value as expr
-      names[j] = mdtab[i+1];			// and access macro as name
+    if (getparamstat(mdtab[i]) & ARGPARAM) {	// if var has assigned value
+      exprs[j] = getparam(mdtab[i]);		// list value given as expr
+      names[j] = mdtab[i+1];			// and name of access macro
       j++;
     }
   exprs[j] = names[j] = NULL;
-  eprintf("[%s: defined %d variable%s]\n", getargv0(), j, j!=1 ? "s" : "");
-  prog = tempnam("/tmp", "sm");
+  prog = mktemp((string) copxstr("/tmp/sm_XXXXXX", sizeof(char)));
   buildmap(prog, names, exprs, NULL,
-	   strnull(getparam("tmap")) ? NULL : getparam("tmap"),
+	   strnull(getparam("t")) ? NULL : getparam("t"),
 	   Precision, NDIM, TRUE);
   execmap(prog);
   if (unlink(prog) != 0)
     error("%s: can't unlink %s\n", getargv0(), prog);
   return (0);
 }
+
 
 //  execmap: use compiled program to map input snap to output snap.
 //  _______________________________________________________________
