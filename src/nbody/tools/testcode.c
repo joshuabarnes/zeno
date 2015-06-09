c73f30838a3a6d3821406b50e12ae4bfd8192ad0
Joshua Barnes <barnes@ifa.hawaii.edu>
2015-06-09 16:32:28 +0900

    Improved error reporting; testcode_hqm has tol parameter.

--- src/nbody/tools/testcode.c
+++ src/nbody/tools/testcode.c
@@ -10,36 +10,51 @@
 #include "phatbody.h"
 #include "gsp.h"
 
+#if !(defined(GSPGRAV) || defined(HQMGRAV))
+#define NBDGRAV
+#endif
+
 string defv[] = {
-#if defined(GSPGRAV)
+#if defined(NBDGRAV)
+				";Integrate test bodies in N-body potential",
+#elif defined(GSPGRAV)
 				";Integrate test bodies in GSP potential",
 #elif defined(HQMGRAV)
 				";Integrate test bodies in Hernquist model",
-#else
-				";Integrate test bodies in N-body potential",
 #endif
   "in=???",			";Initial conditions for test bodies",
   "out=???",			";Output stream of test body snapshots",
-#if defined(GSPGRAV)
+#if defined(NBDGRAV)
+  "grav=???",			";N-body configuration generating potential",
+  "eps=0.025",			";Softening parameter for force calculation",
+  "frozen=true",		";If FALSE, read new grav snap for each step",
+#elif defined(GSPGRAV)
   "grav=???",			";General spherical profile for potential",
 #elif defined(HQMGRAV)
   "M=1.0",			";Total mass of Hernquist model",
   "a=1.0",			";Major axis of Hernquist model",
   "b=1.0",			";Minor axis of Hernquist model",
-#else
-  "grav=???",			";N-body configuration generating potential",
-  "eps=0.025",			";Softening parameter for force calculation",
-  "frozen=true",		";If FALSE, read new grav snap for each step",
+  "tol=1.0e-6",			";Error tolerance for integrals",
 #endif
   "tstop=2.0",			";Duration of integration",
   "dtime=1/32",			";Leap-frog integration time-step",
   "decrit=1.0e-5",		";Initial energy change tolerance",
   "outputs=" PosTag "," VelTag,	";Per-body data arrays written to output",
   "dtout=1/4",			";Time interval between output frames",
-  "VERSION=1.0",		";Josh Barnes  23 May 2015",
+  "VERSION=1.1",		";Josh Barnes  9 June 2015",
   NULL,
 };
 
+#if defined(NBDGRAV)
+local void sumforces(bodyptr btab, int nbody, bodyptr gtab, int ngrav,
+		     real eps2);
+#elif defined(GSPGRAV)
+local void gspforces(bodyptr btab, int nbody, gsprof *gravgsp);
+#elif defined(HQMGRAV)
+local void hqmforces(bodyptr btab, int nbody, real M, real a, real b,
+		     real tol);
+#endif
+
 #define EinitPBF  phatbody[NewBodyFields+0]	// add initial E to bodies
 #define Einit(b)  SelectReal(b,EinitPBF.offset)	// accessor macro for above
 #define EinitTag  "Einit"			// field name for above
@@ -48,24 +63,16 @@ string bodytags[MaxBodyFields] = {
   PosTag, VelTag, MassTag, PhiTag, AccTag, EinitTag, NULL
 };
 
-local void sumforces(bodyptr btab, int nbody, bodyptr gtab, int ngrav,
-		     real eps2);
-
-local void gspforces(bodyptr btab, int nbody, gsprof *gravgsp);
-
-#if defined(HQMGRAV)
-local void hqmforces(bodyptr btab, int nbody, real M, real a, real b);
-#endif
-
 int main(int argc, string argv[])
 {
   stream istr, ostr, gstr;
-  real tnow, tgrav, eps2, tstop, dtime, tout;
+  real tnow, tgrav, eps2, tstop, dtime, tout, Mhqm, ahqm, bhqm, tol;
   real decrit, epot0, demin, demax, derms, de2avg, enow, denow;
   int nbody, ngrav;
   bodyptr btab = NULL, gtab = NULL, bp;
   string bdtags[MaxBodyFields], grtags[MaxBodyFields], *optags;
   gsprof *gravgsp = NULL;
+  bool decrit_inc = FALSE;
 
   initparam(argv, defv);
   new_field(&EinitPBF, RealType, EinitTag);	// define initial energy field
@@ -76,11 +83,7 @@ int main(int argc, string argv[])
     error("%s: can't read input snapshot\n", getprog());
   if (! (set_member(bdtags, PosTag) && set_member(bdtags, VelTag)))
     error("%s: required data missing from input snapshot\n", getprog());
-#if defined(GSPGRAV)
-  gstr = stropen(getparam("grav"), "r");
-  get_history(gstr);
-  gravgsp = get_gsprof(gstr);			// read GSP for grav. field
-#elif !defined(HQMGRAV)
+#if defined(NBDGRAV)
   gstr = stropen(getparam("grav"), "r");
   get_history(gstr);
   if (! get_snap(gstr, &gtab, &ngrav, &tgrav, grtags, FALSE))
@@ -88,6 +91,15 @@ int main(int argc, string argv[])
   if (! (set_member(grtags, MassTag) && set_member(grtags, PosTag)))
     error("%s: required data missing from gravity snapshot\n", getprog());
   eps2 = rsqr(getdparam("eps"));
+#elif defined(GSPGRAV)
+  gstr = stropen(getparam("grav"), "r");
+  get_history(gstr);
+  gravgsp = get_gsprof(gstr);			// read GSP for grav. field
+#elif defined(HQMGRAV)
+  Mhqm = getdparam("M");
+  ahqm = getdparam("a");
+  bhqm = getdparam("b");
+  tol = getdparam("tol");
 #endif
   ostr = stropen(getparam("out"), "w");
   put_history(ostr);
@@ -95,13 +107,14 @@ int main(int argc, string argv[])
   dtime = getdparam("dtime");
   decrit = getdparam("decrit");
   optags = burststring(getparam("outputs"), ",");
-#if defined(GSPGRAV)
-  gspforces(btab, nbody, gravgsp);		// prime the pump...
+#if defined(NBDGRAV)
+  sumforces(btab, nbody, gtab, ngrav, eps2);	// prime the pump...
+#elif defined(GSPGRAV)
+  gspforces(btab, nbody, gravgsp);
 #elif defined(HQMGRAV)
-  hqmforces(btab, nbody, getdparam("M"), getdparam("a"), getdparam("b"));
-#else
-  sumforces(btab, nbody, gtab, ngrav, eps2);
+  hqmforces(btab, nbody, Mhqm, ahqm, bhqm, tol);
 #endif
+
   epot0 = 0.0;					// use as energy scale
   for (bp = btab; bp < NthBody(btab, nbody); bp = NextBody(bp)) {
     epot0 += Phi(bp) / nbody;			// compute avg. potential
@@ -111,7 +124,6 @@ int main(int argc, string argv[])
   put_snap(ostr, &btab, &nbody, &tnow, optags);
   fflush(NULL);
   tout = tnow + getdparam("dtout");
-
   demin = demax = derms = 0.0;			// track maximum errors
   while (tnow < tstop) {			// enter main loop
     for (bp = btab; bp < NthBody(btab, nbody); bp = NextBody(bp)) {
@@ -119,15 +131,15 @@ int main(int argc, string argv[])
       ADDMULVS(Pos(bp), Vel(bp), dtime);	// step positions by dt
     }
     tnow = tnow + dtime;			// step time to new value
-#if defined(GSPGRAV)
-    gspforces(btab, nbody, gravgsp);		// get new accelerations
-#elif defined(HQMGRAV)
-    hqmforces(btab, nbody, getdparam("M"), getdparam("a"), getdparam("b"));
-#else
+#if defined(NBDGRAV)
     if (! getbparam("frozen"))
       if (! get_snap(gstr, &gtab, &ngrav, &tgrav, grtags, TRUE))
 	error("%s: can't read gravity snapshot\n", getprog());
-    sumforces(btab, nbody, gtab, ngrav, eps2);
+    sumforces(btab, nbody, gtab, ngrav, eps2);	// get new accelerations
+#elif defined(GSPGRAV)
+    gspforces(btab, nbody, gravgsp);
+#elif defined(HQMGRAV)
+    hqmforces(btab, nbody, Mhqm, ahqm, bhqm, tol);
 #endif
     de2avg = 0.0;
     for (bp = btab; bp < NthBody(btab, nbody); bp = NextBody(bp)) {
@@ -145,6 +157,7 @@ int main(int argc, string argv[])
 	      getprog(), decrit, tnow,
 	      demin, demax, rsqrt(de2avg), decrit * rsqrt(2.0));
       decrit = decrit * rsqrt(2.0);
+      decrit_inc = TRUE;
     }
     if (tout <= tnow) {
       put_snap(ostr, &btab, &nbody, &tnow, optags);
@@ -152,11 +165,15 @@ int main(int argc, string argv[])
     }
     fflush(NULL);
   }
-  eprintf("[%s: energy error: min,max,rms = %.6g,%.6g,%.6g]\n",
+  eprintf(decrit_inc ?
+	  "[%s: WARNING: energy error: min,max,rms = %.6g,%.6g,%.6g]\n" :
+	  "[%s: energy error: min,max,rms = %.6g,%.6g,%.6g]\n",
 	  getprog(), demin, demax, derms);
   return (0);
 }
 
+#if defined(NBDGRAV)
+
 local void sumforces(bodyptr btab, int nbody, bodyptr gtab, int ngrav,
 		     real eps2)
 {
@@ -182,6 +199,8 @@ local void sumforces(bodyptr btab, int nbody, bodyptr gtab, int ngrav,
     SETV(Acc(bp), acc0);
   }
 }
+
+#endif
 
 #if defined(GSPGRAV)
 
@@ -210,7 +229,7 @@ local void gspforces(bodyptr btab, int nbody, gsprof *gravgsp)
 #define R(par)   (((double *) par)[2])
 #define z(par)   (((double *) par)[3])
 
-local double phiint(double u, void *par)
+local double intPhi(double u, void *par)
 {
   real Delta, mu;
 
@@ -219,7 +238,7 @@ local double phiint(double u, void *par)
   return (0.5 / (Delta * rsqr(1 + mu)));
 }
 
-local double aRint(double u, void *par)
+local double int_aR(double u, void *par)
 {
   real Delta, mu;
 
@@ -228,7 +247,7 @@ local double aRint(double u, void *par)
   return (R(par) / ((a2(par) + u) * Delta * mu * rqbe(1 + mu)));
 }
 
-local double azint(double u, void *par)
+local double int_az(double u, void *par)
 {
   real Delta, mu;
 
@@ -237,15 +256,15 @@ local double azint(double u, void *par)
   return (z(par) / ((b2(par) + u) * Delta * mu * rqbe(1 + mu)));
 }
 
-#define AEPS  1.0e-6
-#define REPS  1.0e-6
-
-local void hqmforces(bodyptr btab, int nbody, real M, real a, real b)
+local void hqmforces(bodyptr btab, int nbody, real M, real a, real b,
+		     real tol)
 {
   bodyptr bp;
-  double r, mr3i, params[4], phiI, phiE, aRI, aRE, azI, azE;
+  double r, mr3i, params[4], phi0, aR0, az0, abserr[3];
   static gsl_integration_workspace *wksp = NULL;
-  gsl_function phiF, aRF, azF;
+  gsl_function FPhi, F_aR, F_az;
+  static double maxerr = 0.0;
+  int stat[3];
 
   if (a == b) {					// spherical case is easy!
     for (bp = btab; bp < NthBody(btab, nbody); bp = NextBody(bp)) {
@@ -255,26 +274,36 @@ local void hqmforces(bodyptr btab, int nbody, real M, real a, real b)
       MULVS(Acc(bp), Pos(bp), - mr3i);
     }
   } else {					// flattened case is harder
-    if (wksp == NULL) {				// first call; initialze
+    if (wksp == NULL) {				// on first call, initialze
       wksp = gsl_integration_workspace_alloc(1000);
-      gsl_set_error_handler_off();		// live dangerously
+      gsl_set_error_handler_off();		// handle errors below
     }
-    phiF.function = &phiint;
-    aRF.function = &aRint;
-    azF.function = &azint;
-    phiF.params = aRF.params = azF.params = params;
+    FPhi.function = &intPhi;
+    F_aR.function = &int_aR;
+    F_az.function = &int_az;
+    FPhi.params = F_aR.params = F_az.params = params;
     a2(params) = rsqr(a);
     b2(params) = rsqr(b);
     for (bp = btab; bp < NthBody(btab, nbody); bp = NextBody(bp)) {
       R(params) = rsqrt(rsqr(Pos(bp)[0]) + rsqr(Pos(bp)[1]));
       z(params) = Pos(bp)[2];
-      gsl_integration_qagiu(&phiF, 0.0, AEPS, REPS, 1000, wksp, &phiI, &phiE);
-      gsl_integration_qagiu(&aRF, 0.0, AEPS, REPS, 1000, wksp, &aRI, &aRE);
-      gsl_integration_qagiu(&azF, 0.0, AEPS, REPS, 1000, wksp, &azI, &azE);
-      Phi(bp) = - M * phiI;
-      Acc(bp)[0] = - M * (Pos(bp)[0] / R(params)) * aRI;
-      Acc(bp)[1] = - M * (Pos(bp)[1] / R(params)) * aRI;
-      Acc(bp)[2] = - M * azI;
+      stat[0] = gsl_integration_qagiu(&FPhi, 0.0, tol, 0.0,
+				      1000, wksp, &phi0, &abserr[0]);
+      stat[1] = gsl_integration_qagiu(&F_aR, 0.0, tol, 0.0,
+				      1000, wksp, &aR0, &abserr[1]);
+      stat[2] = gsl_integration_qagiu(&F_az, 0.0, tol, 0.0,
+				      1000, wksp, &az0, &abserr[2]);
+      if (stat[0] || stat[1] || stat[2])	// any errors reported?
+	for (int i = 0; i < 3; i++)
+	  if (stat[i] != 0 && abserr[i] > maxerr) {
+	    eprintf("[%s.hqmforces: warning: %s  abserr[%d] = %g]\n",
+		    getprog(), gsl_strerror(stat[i]), i+1, abserr[i]);
+	    maxerr = abserr[i];			// adjust reporting threshold
+	  }
+      Phi(bp) = - M * phi0;
+      Acc(bp)[0] = - M * (Pos(bp)[0] / R(params)) * aR0;
+      Acc(bp)[1] = - M * (Pos(bp)[1] / R(params)) * aR0;
+      Acc(bp)[2] = - M * az0;
     }
   }
 }
