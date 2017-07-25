d7b655000a2c277f1be56c6bbef63cc0904a9dad
Joshua Barnes <barnes@ifa.hawaii.edu>
2017-07-24 22:39:11 -1000

    Revision, tracking changes with gsp.h

--- src/nbody/gspmodels/gspeval.c
+++ src/nbody/gspmodels/gspeval.c
@@ -1,5 +1,5 @@
 /*
- * GSPEVAL.C: evaluate GSP at particle positions.
+ * gspeval.c: evaluate GSP at particle positions.
  */
 
 #include "stdinc.h"
@@ -10,55 +10,50 @@
 #include "gsp.h"
 
 string defv[] = {		";Evaluate GSP at particle positions",
-    "gsp=???",			";Input GSP file",
-    "in=???",			";Input snapshot file",
-    "out=???",			";Output snapshot file",
-    "option=rho",		";Other choices: drho, mass, phi",
-    "VERSION=1.0",		";Josh Barnes  6 June 2007",
-    NULL,
+  "gsp=???",			";Input GSP file",
+  "in=???",			";Input snapshot file",
+  "out=???",			";Output snapshot file",
+  "option=rho",			";Other choices: grad, mass, phi",
+  "VERSION=2.0",		";Josh Barnes  21 June 2017",
+  NULL,
 };
-
-string bodyfields[] = { AuxTag, NULL };
 
 int main(int argc, string argv[])
 {
-  stream fstr, istr, ostr;
+  string bodyfields[] = { AuxTag, NULL }, intags[MaxBodyFields];
+  stream gstr, istr, ostr;
   gsprof *gsp;
   bodyptr btab = NULL, p;
   int nbody;
-  real tnow, r;
-  string intags[MaxBodyFields];
+  real tnow;
 
   initparam(argv, defv);
   layout_body(bodyfields, Precision, NDIM);
-  fstr = stropen(getparam("gsp"), "r");
-  get_history(fstr);
-  gsp = get_gsprof(fstr);
+  gstr = stropen(getparam("gsp"), "r");
+  get_history(gstr);
+  gsp = gsp_read(gstr);
   istr = stropen(getparam("in"), "r");
   get_history(istr);
-  if (! get_snap(istr, &btab, &nbody, &tnow, intags, TRUE))
-    error("%s: snapshot input failed\n", getargv0());
-  if (! set_member(intags, PosTag))
-    error("%s: position data missing\n", getargv0());
+  if (! (get_snap(istr, &btab, &nbody, &tnow, intags, TRUE, NULL) &&
+	 set_member(intags, PosTag)))
+    error("%s: snapshot input failed or lacked positions\n", getprog());
   if (streq(getparam("option"), "rho"))
     for (p = btab; p < NthBody(btab, nbody); p = NextBody(p))
-      Aux(p) = rho_gsp(gsp, absv(Pos(p)));
-  else if (streq(getparam("option"), "drho"))
+      Aux(p) = gsp_rho(gsp, absv(Pos(p)));
+  else if (streq(getparam("option"), "grad"))
     for (p = btab; p < NthBody(btab, nbody); p = NextBody(p))
-      Aux(p) = drho_gsp(gsp, absv(Pos(p)));
+      Aux(p) = gsp_grad(gsp, absv(Pos(p)));
   else if (streq(getparam("option"), "mass"))
     for (p = btab; p < NthBody(btab, nbody); p = NextBody(p))
-      Aux(p) = mass_gsp(gsp, absv(Pos(p)));
+      Aux(p) = gsp_mass(gsp, absv(Pos(p)));
   else if (streq(getparam("option"), "phi"))
     for (p = btab; p < NthBody(btab, nbody); p = NextBody(p))
-      Aux(p) = phi_gsp(gsp, absv(Pos(p)));
+      Aux(p) = gsp_phi(gsp, absv(Pos(p)));
   else 
-    error("%s: unknown option %s\n", getargv0(), getparam("option"));
-  if (! strnull(getparam("out"))) {
-    ostr = stropen(getparam("out"), "w");
-    put_history(ostr);
-    put_snap(ostr, &btab, &nbody, &tnow, set_union(bodyfields, intags));
-    strclose(ostr);
-  }
-  return (0);
+    error("%s: unknown option %s\n", getprog(), getparam("option"));
+  ostr = stropen(getparam("out"), "w");
+  put_history(ostr);
+  put_snap(ostr, &btab, &nbody, &tnow, set_union(bodyfields, intags));
+  fflush(NULL);
+  return 0;
 }
