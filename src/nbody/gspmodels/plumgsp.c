fede71112c381ef754f3bfc1da49f1cea873ea18
Joshua Barnes <barnes@ifa.hawaii.edu>
2017-07-24 22:26:32 -1000

    Revision tracking changes to gsp.h, including new name for library routine and test code for density, gradient, mass, potential.

--- src/nbody/gspmodels/plumgsp.c
+++ src/nbody/gspmodels/plumgsp.c
@@ -1,74 +1,131 @@
 /*
- * PLUMGSP.C: generate profile tables for Plummer model.
+ * plumgsp.c: generate profile tables for Plummer model.
  */
 
 #include "stdinc.h"
 #include "mathfns.h"
-#include "assert.h"
+#include "getparam.h"
 #include "gsp.h"
 
-/*
- * PLUMGSP: initialize tables for Plummer model.
- */
+//  Functions defining Plummer models.  First argument holds parameters.
 
-gsprof *plumgsp(real mtot, real a, int np, real rmin, real rmax)
+local double density(void *p, double r);
+local double gradient(void *p, double r);
+local double mass(void *p, double r);
+local double potential(void *p, double r);
+
+//  pars: structure used to pass parameters.
+//  ________________________________________
+
+typedef struct {
+  double mtot;
+  double ascl;
+} pars;
+
+//  gsp_plum: initialize tables for Plummer model.
+//  ______________________________________________
+
+gsprof *gsp_plum(double mtot, double ascl,
+		  int np, double rmin, double rmax)
 {
-    gsprof *gsp;
-    real *rtab, *dtab, *mtab, lgrs;
-    int i;
+  gsprof *gsp = (gsprof *) allocate(sizeof(gsprof));
+  double lgrs;
+  pars p = { mtot, ascl };
 
-    assert(mtot > 0.0 && a > 0.0);
-    gsp = (gsprof *) allocate(sizeof(gsprof));
-    rtab = (real *) allocate(np * sizeof(real));
-    dtab = (real *) allocate(np * sizeof(real));
-    mtab = (real *) allocate(np * sizeof(real));
-    lgrs = rlog2(rmax / rmin) / (np - 1);
-    for (i = 0; i < np; i++) {
-	rtab[i] = rmin * rexp2(lgrs * i);
-	dtab[i] = (3 / FOUR_PI) * mtot * a*a /
-		    rpow(rsqr(rtab[i]) + a*a, 2.5);
-	mtab[i] = mtot * rqbe(rtab[i]) /
-		    rpow(rsqr(rtab[i]) + a*a, 1.5);
-    }
-    gsp->npoint = np;
-    gsp->radius = rtab;
-    gsp->density = dtab;
-    gsp->mass = mtab;
-    gsp->alpha = 0.0;
-    gsp->beta = -5.0;
-    gsp->mtot = mtot;
-    return (gsp);
+  gsp->npoint = np;
+  gsp->radius  = (double *) allocate(np * sizeof(double));
+  gsp->density = (double *) allocate(np * sizeof(double));
+  gsp->mass    = (double *) allocate(np * sizeof(double));
+  // gsp->phi     = (double *) allocate(np * sizeof(double));
+  lgrs = log2(rmax / rmin) / (np - 1);
+  eprintf("[%s.gsp_plum: lgrs = %f (%a)]\n", getprog(), lgrs, lgrs);
+  for (int i = 0; i < np; i++) {
+    gsp->radius[i]  = rmin * exp2(lgrs * i);
+    gsp->density[i] = density(&p, gsp->radius[i]);
+    gsp->mass[i]    = mass(&p, gsp->radius[i]);
+    if (i > 0 && gsp->mass[i] == gsp->mass[i-1])
+      error("%s.gsp_plum: mass degenerate (i = %d)\n", getprog(), i);
+    // gsp->phi[i]     = potential(&p, gsp->radius[i]);
+  }
+  gsp->alpha = 0.0;
+  gsp->beta = -5.0;
+  gsp->mtot = mtot;
+  gsp_test_rad(gsp, gsp_rho, density, &p, "density");
+  gsp_test_rad(gsp, gsp_grad, gradient, &p, "gradient");
+  gsp_test_rad(gsp, gsp_mass, mass, &p, "mass");
+  gsp_test_rad(gsp, gsp_phi, potential, &p, "potential");
+  return gsp;
 }
 
-#ifdef TESTBED
+//  Access macros for parameters.
 
-#include "getparam.h"
+#define _mtot  (((pars *) p)->mtot)
+#define _ascl  (((pars *) p)->ascl)
+
+//  density: compute space density as function of radius.
+//  _____________________________________________________
+
+local double density(void *p, double r)
+{
+  return ((3 / (4 * M_PI)) * _mtot * _ascl*_ascl /
+	  pow(r*r + _ascl*_ascl, 2.5));
+
+}
+
+//  gradient: compute density gradient as function of radius.
+//  _________________________________________________________
+
+local double gradient(void *p, double r)
+{
+  return (- (15 / (4 * M_PI)) * _mtot * _ascl*_ascl * r /
+	  pow(r*r + _ascl*_ascl, 3.5));
+}
+
+//  mass: compute enclosed mass as function of radius.
+//  __________________________________________________
+
+local double mass(void *p, double r)
+{
+  return (_mtot * r*r*r / pow(r*r + _ascl*_ascl, 1.5));
+}
+
+//  potential: compute potential as function of radius.
+//  ___________________________________________________
+
+local double potential(void *p, double r)
+{
+  return (- _mtot / sqrt(r*r + _ascl*_ascl));
+}
+
+#ifdef UTILITY
 
 string defv[] = {		";Generate profile for Plummer model",
-    "out=???",			";Output file for profile tables",
-    "mtot=1.0",			";Total mass of model",
-    "a=1.0",			";Length scale of model",
-    "npoint=257",		";Number of points in tables",
-    "rrange=1/4096:16",		";Range of radii tabulated",
-    "VERSION=1.2",		";Josh Barnes  4 February 1995",
-    NULL,
+  "out=",			";Output file for profile tables",
+  "mtot=1.0",			";Total mass of model",
+  "a=1.0",			";Length scale of model",
+  "npoint=1281",		";Number of points in tables",
+  "rrange=1/1024:1024",		";Range of radii tabulated",
+  "VERSION=2.0",		";Josh Barnes  28 May 2017",
+  NULL,
 };
 
 int main(int argc, string argv[])
 {
-    real rrange[2];
-    gsprof *gsp;
-    stream ostr;
+  real rrange[2];
+  gsprof *gsp;
+  stream ostr;
 
-    initparam(argv, defv);
-    setrange(rrange, getparam("rrange"));
-    gsp = plumgsp(getdparam("mtot"), getdparam("a"),
-		  getiparam("npoint"), rrange[0], rrange[1]);
+  initparam(argv, defv);
+  setrange(rrange, getparam("rrange"));
+  gsp = gsp_plum(getdparam("mtot"), getdparam("a"),
+		 getiparam("npoint"), rrange[0], rrange[1]);
+  if (! strnull(getparam("out"))) {
     ostr = stropen(getparam("out"), "w");
     put_history(ostr);
-    put_gsprof(ostr, gsp);
-    strclose(ostr);
-    return (0);
+    gsp_write(ostr, gsp);
+  }
+  fflush(NULL);
+  return 0;
 }
 
 #endif
