be26eec4edacfd6778f8c13a193500a12c242a76
Joshua Barnes <barnes@ifa.hawaii.edu>
2017-09-30 12:34:19 -1000

    Added Bessel J functions.

--- src/lib/clib/bessel.c
+++ src/lib/clib/bessel.c
@@ -6,7 +6,50 @@
 #include "getparam.h"
 
 #include <gsl/gsl_sf_bessel.h>
+#include <gsl/gsl_errno.h>
 
+//  bessel_J0: regular Bessel function J, order zero.
+//  _________________________________________________
+
+double bessel_J0(double x)
+{
+  gsl_sf_result res;
+  int stat;
+
+  stat = gsl_sf_bessel_J0_e(x, &res);
+  if (stat != GSL_SUCCESS)
+    error("%s.bessel_J0: error status: %s\n", getprog(), gsl_strerror(stat));
+  return (res.val);
+}
+
+//  bessel_J1: regular Bessel function J, order one.
+//  ________________________________________________
+
+double bessel_J1(double x)
+{
+  gsl_sf_result res;
+  int stat;
+
+  stat = gsl_sf_bessel_J1_e(x, &res);
+  if (stat != GSL_SUCCESS)
+    error("%s.bessel_J1: error status: %s\n", getprog(), gsl_strerror(stat));
+  return (res.val);
+}
+
+//  bessel_J: regular Bessel function J, order nu.
+//  ______________________________________________
+
+double bessel_J(int n, double x)
+{
+  gsl_sf_result res;
+  int stat;
+
+  stat = gsl_sf_bessel_Jn_e(n, x, &res);
+  if (stat != GSL_SUCCESS)
+    error("%s.bessel_J: error status: %s\n", getprog(), gsl_strerror(stat));
+  return (res.val);
+}
+
 //  bessel_I0: regular modified cylindrical Bessel function I, order zero.
 //  ______________________________________________________________________
 
@@ -16,7 +59,7 @@ double bessel_I0(double x)
   int stat;
 
   stat = gsl_sf_bessel_I0_e(x, &res);
-  if (stat != 0)
+  if (stat != GSL_SUCCESS)
     error("%s.bessel_I0: error status: %s\n", getprog(), gsl_strerror(stat));
   return (res.val);
 }
@@ -30,7 +73,7 @@ double bessel_I1(double x)
   int stat;
 
   stat = gsl_sf_bessel_I1_e(x, &res);
-  if (stat != 0)
+  if (stat != GSL_SUCCESS)
     error("%s.bessel_I1: error status: %s\n", getprog(), gsl_strerror(stat));
   return (res.val);
 }
@@ -44,7 +87,7 @@ double bessel_I(double nu, double x)
   int stat;
 
   stat = gsl_sf_bessel_Inu_e(nu, x, &res);
-  if (stat != 0)
+  if (stat != GSL_SUCCESS)
     error("%s.bessel_I: error status: %s\n", getprog(), gsl_strerror(stat));
   return (res.val);
 }
@@ -58,7 +101,7 @@ double bessel_K0(double x)
   int stat;
 
   stat = gsl_sf_bessel_K0_e(x, &res);
-  if (stat != 0)
+  if (stat != GSL_SUCCESS)
     error("%s.bessel_K0: error status: %s\n", getprog(), gsl_strerror(stat));
   return (res.val);
 }
@@ -72,7 +115,7 @@ double bessel_K1(double x)
   int stat;
 
   stat = gsl_sf_bessel_K1_e(x, &res);
-  if (stat != 0)
+  if (stat != GSL_SUCCESS)
     error("%s.bessel_K1: error status: %s\n", getprog(), gsl_strerror(stat));
   return (res.val);
 }
@@ -86,7 +129,7 @@ double bessel_K(double nu, double x)
   int stat;
 
   stat = gsl_sf_bessel_Knu_e(nu, x, &res);
-  if (stat != 0)
+  if (stat != GSL_SUCCESS)
     error("%s.bessel_k: error status: %s\n", getprog(), gsl_strerror(stat));
   return (res.val);
 }
@@ -97,28 +140,34 @@ double bessel_K(double nu, double x)
 
 string defv[] = {		";Test bessel functions",
   "x=4.5",			";Argument for functions",
-  "nu=0.5",			";Order for functions",
-  "VERSION=1.0",		";Joshua Barnes  11 May 2012",    
+  "n=1",			";Order for function J()",
+  "nu=0.5",			";Order for functions I(), K()",
+  "VERSION=1.1",		";Joshua Barnes  22 June 2017",    
   NULL,
 };
 
 int main(int argc, string argv[])
 {
   double x, nu, ex;
+  int n;
 
   initparam(argv, defv);
   x = getdparam("x");
+  n = getiparam("n");
   nu = getdparam("nu");
   ex = exp(x);
+  printf("bessel_J0(%f) = %f\n", x, bessel_J0(x));
+  printf("bessel_J1(%f) = %f\n", x, bessel_J1(x));
+  printf("bessel_J(%d,%f) = %f\n", n, x, bessel_J(n, x));
   printf("bessel_I0(%f) = %f\t%f\n", x, bessel_I0(x), bessel_I0(x) / ex);
   printf("bessel_I1(%f) = %f\t%f\n", x, bessel_I1(x), bessel_I1(x) / ex);
-  printf("bessel_K0(%f) = %f\t%f\n", x, bessel_K0(x), ex * bessel_K0(x));
-  printf("bessel_K1(%f) = %f\t%f\n", x, bessel_K1(x), ex * bessel_K1(x));
   printf("bessel_I(%f,%f) = %f\t%f\n",
 	 nu, x, bessel_I(nu, x), bessel_I(nu, x) / ex);
+  printf("bessel_K0(%f) = %f\t%f\n", x, bessel_K0(x), ex * bessel_K0(x));
+  printf("bessel_K1(%f) = %f\t%f\n", x, bessel_K1(x), ex * bessel_K1(x));
   printf("bessel_K(%f,%f) = %f\t%f\n",
-	 nu, x, bessel_K(nu, x), bessel_K(nu, x) / ex);
-  return (0);
+	 nu, x, bessel_K(nu, x), ex * bessel_K(nu, x));
+  return 0;
 }
 
 #endif
