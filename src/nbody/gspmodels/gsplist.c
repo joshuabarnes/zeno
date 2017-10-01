93d985bd058c5f36a944731dc1977eb2b2fd1154
Joshua Barnes <barnes@ifa.hawaii.edu>
2017-09-30 15:02:27 -1000

    Updated to use new gsp functions.

--- src/nbody/gspmodels/gsplist.c
+++ src/nbody/gspmodels/gsplist.c
@@ -13,7 +13,7 @@ string defv[] = {		";List general spherical profile",
   "beta_a=0.0",			";Anisotropy parameter: beta_a <= 1",
   "npoint=17",			";Number of points to list",
   "rrange=1/256:256",		";Range of radii to list",
-  "VERSION=1.1",		";Josh Barnes  31 January 2015",
+  "VERSION=2.0",		";Josh Barnes  30 September 2017",
   NULL,
 };
 
@@ -21,18 +21,19 @@ int main(int argc, string argv[])
 {
   stream istr;
   gsprof *gsp, *ggsp;
-  real beta_a, *sig2, rrange[2], lgrs, r;
+  double beta_a, lgrs, r;
+  real rrange[2];
   int np, i;
 
   initparam(argv, defv);
   istr = stropen(getparam("gsp"), "r");
   get_history(istr);
-  gsp = get_gsprof(istr);
+  gsp = gsp_read(istr);
   strclose(istr);
   if (! strnull(getparam("grav"))) {
     istr = stropen(getparam("grav"), "r");
     get_history(istr);
-    ggsp = get_gsprof(istr);
+    ggsp = gsp_read(istr);
     strclose(istr);
   } else
     ggsp = gsp;
@@ -40,15 +41,15 @@ int main(int argc, string argv[])
   np = getiparam("npoint");
   setrange(rrange, getparam("rrange"));
   lgrs = np > 1 ? rlog2(rrange[1] / rrange[0]) / (np - 1) : 0.0;
-  (void) phi_gsp(ggsp, rrange[0]);		// precalculate phi table
-  sig2 = calc_sig2_gsp(gsp, ggsp, beta_a);
+  gsp_calc_phi(ggsp);
+  gsp_calc_sig2(gsp, ggsp, beta_a);
   printf("#%12s%13s%13s%13s%13s%13s\n",
 	 "radius", "rho", "drho/dr", "mass", "phi", "sig^2");
   for (i = 0; i < np; i++) {
     r = rrange[0] * rexp2(lgrs * i);
     printf("%13.4e%13.4e%13.4e%13.4e%13.7f%13.4e\n", r,
-	   rho_gsp(gsp, r), drho_gsp(gsp, r), mass_gsp(gsp, r),
-	   phi_gsp(ggsp, r), sig2_gsp(gsp, ggsp, beta_a, sig2, r));
+	   gsp_rho(gsp, r), gsp_grad(gsp, r), gsp_mass(gsp, r),
+	   gsp_phi(ggsp, r), gsp_sig2(gsp, r));
   }
   return (0);
 }
